07e4d89ee8e01a35c81478a0c82270cf7ddb548e
xmisao <mail@xmisao.com>
2016-03-10 00:52:53 +0900

    add logging and message output

--- wliconfig.rb
+++ wliconfig.rb
@@ -1,6 +1,7 @@
 require 'optparse'
 require 'yaml'
 require 'mechanize'
+require 'logger'
 
 module WLIConfig
   #
@@ -19,21 +20,30 @@ module WLIConfig
       end
 
       def change_config(config)
+        # print url
+        $logger.info("Attempt to access to '#{@url}'.")
+
         agent = create_agent()
 
         # fetch index page
+        $logger.info("Begin fetch index page.")
         page = agent.get(@url)
         page = fetch_main_iframe(page)
         page = page.links_with(:href => 'index.html?page=wizard_func_wlan_sta_crypto.html')[0].click
+        $logger.info("End fetch index page.")
 
         # fetch config page and submit config
+        $logger.info("Begin fetch config page and submit config.")
         page = fetch_main_iframe(page)
         form = page.forms[0]
         fill_config_form(form, config)
         page = form.submit
+        $logger.info("End fetch ocnfig page and submit config.")
 
         # fetch finish page
+        $logger.info("Begin fetch complete page.")
         page = page.forms[0].submit
+        $logger.info("End fetch complete page.")
       end
 
       def create_agent()
@@ -71,7 +81,9 @@ module WLIConfig
     attr_accessor :product, :addr, :user, :pass, :wlan_ssid, :wlan_mode, :wlan_key
 
     def update_from_file(fname)
-      if fname and FileTest.exist?(fname)
+      return unless fname
+      if FileTest.exist?(fname)
+        $logger.info("Configuration file #{fname} does exist.")
         open(fname){|f|
           yaml = YAML.load(f.read)
           @addr ||= yaml["addr"]
@@ -83,6 +95,7 @@ module WLIConfig
         }
         true
       else
+        $logger.warn("Configuration file #{fname} does not exist.")
         false
       end
     end
@@ -127,13 +140,18 @@ module WLIConfig
   def create_option_parser(container)
     op = OptionParser.new
 
-    op.on('-f', 'Read options from specified YAML file. In default, read "$HOME/.wliutx" if exist.'){|v| container[:fname] = v}
+
+    op.banner = "wliconfig -- The 3rd party configuration cli for BUFFALO INC wireless lan adopters are 'WLI' series.\n\nUsage: wliconfig [options...]"
+    op.version = "1.0.0"
+
+    op.on('-f FILE', 'Read options from specified YAML file.'){|v| container[:fname] = v}
     op.on('-a', '--addr ADDR', 'WLI product\'s ip address. (eg. 192.168.0.1)'){|v| container[:addr] = v}
     op.on('-u', '--user USERNAME', 'Basic auth username. (eg. admin)'){|v| container[:user] = v}
     op.on('-p', '--pass PASSWORD', 'Basic auth password. (eg. password)'){|v| container[:pass] = v}
     op.on('-s', '--wlan-ssid SSID', 'SSID that to connect wireless lan.'){|v| container[:wlan_ssid] = v}
     op.on('-m', '--wlan-mode MODE', 'Auth mode that to connect wireless lan. (none, wep_hex, wep_char, tkip, aes, wpa2_tkip or wpa2_aes is valid.)'){|v| container[:wlan_mode] = v}
     op.on('-k', '--wlan-key KEY', 'Key or pass phrase that to connect wireless lan.'){|v| container[:wlan_key] = v}
+    op.on('--debug', 'For developers only. Enabled debug mode.'){|v| $debug = true}
 
     op
   end
@@ -149,15 +167,44 @@ module WLIConfig
   end
 end
 
-include WLIConfig
+$logger = Logger.new(STDOUT)
 
-container = {}
+def main(argv)
+  include WLIConfig
 
-parser = create_option_parser(container)
-parser.parse(ARGV)
+  container = {}
+  parser = create_option_parser(container)
+  parser.parse(argv)
 
-options = create_options(container)
+  unless $debug
+    $logger.level = Logger::INFO
+    $logger.formatter = proc{|severity, datetime, progname, msg| "#{msg}\n"}
+  end
 
-product = ProductFactory.create(options.product, options.addr, options.user, options.pass)
-config = WLANConfig.new(options.wlan_ssid, options.wlan_mode, options.wlan_key)
-product.change_config(config)
+  $logger.info("Start processing...")
+
+  $logger.debug("container:" + container.inspect)
+
+  options = create_options(container)
+  $logger.debug("options:" + options.inspect)
+
+  product = ProductFactory.create(options.product, options.addr, options.user, options.pass)
+  $logger.debug("product:" + product.inspect)
+
+  config = WLANConfig.new(options.wlan_ssid, options.wlan_mode, options.wlan_key)
+  $logger.debug("config:" + config.inspect)
+
+  product.change_config(config)
+
+  $logger.info("Complete processing successfully.")
+
+  0
+rescue
+  $logger.error($!)
+  $logger.error("Processing failure.")
+
+  1
+end
+
+ret = main(ARGV)
+exit ret
